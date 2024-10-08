d97eb31f7fc64fc5651ee230458dbe41f9d42b0e
Iker Duque Delgado <iker_duque@euneiz.com>
2024-10-08 17:13:23 +0200

    Disparo y giro de camara

---
+++ Assets/CambiarRotacion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarRotacion : MonoBehaviour
+{
+    [Header("MovimientoCamara")]
+    private Vector3 objetivo;
+    [SerializeField] private Camera camara;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        objetivo = camara.ScreenToWorldPoint(Input.mousePosition);
+
+        float anguloRadianes = Mathf.Atan2(objetivo.y - transform.position.y, objetivo.x - transform.position.x);
+        float anguloGrados = (180 / Mathf.PI) * anguloRadianes - 90;
+        transform.rotation = Quaternion.Euler(0, 0, anguloGrados); 
+            }
+}
